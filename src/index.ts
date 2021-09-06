14c6ecc2aba82305cdc68181dc3068eac4da9898
FabijanC <fabijan.corak@gmail.com>
2021-09-06 14:17:48 +0200

    Add compile and deploy tasks

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ function createVenv(pythonCommand: string, venvDir: string) {
     const pipPath = path.join(venvDir, "bin", "pip3");
     if (!fs.existsSync(pipPath)) {
         console.log(`Creating venv in ${venvDir}`);
-        if (spawnSync(pythonCommand, ["-m", "venv", venvDir]).error) {
+        if (spawnSync(pythonCommand, ["-m", "venv", venvDir]).status) {
             throw new Error("Could not create venv");
         }
     }
@@ -48,8 +48,75 @@ function createVenv(pythonCommand: string, venvDir: string) {
 
 function installDependencies(pipPath: string, dependencies: string[]) {
     console.log("Checking dependencies");
-    if (spawnSync(pipPath, ["install"].concat(dependencies)).error) {
+    if (spawnSync(pipPath, ["install"].concat(dependencies)).status) {
         throw new Error("Installing dependencies failed");
+    }
+}
+
+function traverseFiles(
+    traversable: string,
+    predicate: (path: string) => boolean,
+    action: (path: string) => void
+): void {
+    const stats = fs.lstatSync(traversable);
+    if (stats.isDirectory()) {
+        for (const childName of fs.readdirSync(traversable)) {
+            const childPath = path.join(traversable, childName);
+            traverseFiles(childPath, predicate, action);
+        }
+    } else if (stats.isFile()) {
+        if (predicate(traversable)) {
+            action(traversable);
+        }
+    } else {
+        throw new Error(`Can only interpret files and directories. ${traversable} is neither.`);
+    }
+}
+
+function hasCairoExtension(filePath: string) {
+    return path.extname(filePath) === ".cairo";
+}
+
+function hasStarknetDeclaration(filePath: string) {
+    const content = fs.readFileSync(filePath).toString();
+    for (const line of content.split("\n")) {
+        if (line.trimRight() === "%lang starknet") {
+            return true;
+        }
+    }
+    return false;
+}
+
+function hasCairoJsonExtension(filePath: string) {
+    return filePath.endsWith(".cairo.json");
+}
+
+function isSimpleCairo(filePath: string) {
+    return hasCairoExtension(filePath) && !hasStarknetDeclaration(filePath);
+}
+
+function isStarknetContract(filePath: string) {
+    return hasCairoExtension(filePath) && hasStarknetDeclaration(filePath);
+}
+
+function getCompileFunction(compilerPath: string, contractsPath: string, artifactsPath: string) {
+    const root = path.dirname(contractsPath);
+    const rootRegex = new RegExp("^" + root);
+    return (file: string) => {
+        const suffix = file.replace(rootRegex, "");
+        const outputPath = path.join(artifactsPath, suffix) + ".json";
+        const compileArgs = [file, "--output", outputPath]; // TODO abi
+
+        console.log("Compiling", file);
+        const executed = spawnSync(compilerPath, compileArgs);
+        for (const logLine of executed.output) {
+            if (logLine) {
+                console.log(logLine.toString());
+            }
+        }
+
+        const finalMsg = executed.status ? "Compilation failed" : "Compilation succeeded";
+        console.log(`\t${finalMsg}\n`);
     }
 }
 
@@ -64,21 +131,41 @@ function main() {
     installDependencies(paths.pipPath, ["ecdsa", "fastecdsa", "sympy", "cairo-lang"]);
 
     task("cairo-compile", "Compiles programs written in Cairo")
-        .setAction((args) => {
-            spawnSync(paths.cairoCompilePath);
-            return null;
+        .setAction(async (_args, hre) => {
+            const sourcesPath = hre.config.paths.sources;
+            const artifactsPath = hre.config.paths.artifacts;
+            const compileFunction = getCompileFunction(paths.cairoCompilePath, sourcesPath, artifactsPath);
+            traverseFiles(sourcesPath, isSimpleCairo, compileFunction);
         });
-    
+
     task("starknet-compile", "Compiles StarkNet contracts")
-        .setAction((args) => {
-            spawnSync(paths.starknetCompilePath);
-            return null;
+        .setAction(async (_args, hre) => {
+            const sourcesPath = hre.config.paths.sources;
+            const artifactsPath = hre.config.paths.artifacts;
+            const compileFunction = getCompileFunction(paths.starknetCompilePath, sourcesPath, artifactsPath);
+            traverseFiles(sourcesPath, isStarknetContract, compileFunction);
         });
 
     task("starknet-deploy", "Deploys Starknet contracts")
-        .setAction((args) => {
-            spawnSync(paths.starknetPath);
-            return null;
+        .addFlag("alpha", "Use the alpha version of testnet")
+        .setAction(async (args, hre) => {
+            traverseFiles(hre.config.paths.artifacts, hasCairoJsonExtension, file => {
+                // TODO check not to deploy simple cairo
+                console.log("Deploying", file);
+                const starknetArgs = ["deploy", "--contract", file];
+                if (args.alpha) {
+                    starknetArgs.push("--network=alpha");
+                }
+                const executed = spawnSync(paths.starknetPath, starknetArgs);
+                for (const logLine of executed.output) {
+                    if (logLine) {
+                        console.log(logLine.toString());
+                    }
+                }
+
+                const finalMsg = executed.status ? "Deployment failed" : "Deployment succeeded";
+                console.log(`\t${finalMsg}\n`);
+            })
         });
 }
 
