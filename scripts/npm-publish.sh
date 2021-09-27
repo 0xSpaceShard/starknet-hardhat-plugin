2002cc92cf618843e019440c67e64bf405b763d0
FabijanC <fabijan.corak@gmail.com>
2021-09-27 13:35:17 +0200

    Add CI

---
+++ scripts/npm-publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+CORE_LOCAL_VERSION=$(jq -r ".version" package.json)
+CORE_NPM_VERSION=$(npm view @shardlabs/starknet-hardhat-plugin dist-tags.latest)
+
+if [ $CORE_LOCAL_VERSION = $CORE_NPM_VERSION ]; then
+  echo "Latest npm version is equal to current package version. Up the version to publish to npm."
+else
+  npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
+  npm install
+  npm run build
+  npm publish --verbose --access=public
+fi
