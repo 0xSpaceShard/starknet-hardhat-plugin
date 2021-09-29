3fc9b2df6692de42e9f1dc1448ae1d53fa58f045
FabijanC <fabijan.corak@gmail.com>
2021-09-29 13:59:49 +0200

    Add context to publish job

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -7,8 +7,8 @@ CORE_NPM_VERSION=$(npm view @shardlabs/starknet-hardhat-plugin dist-tags.latest)
 if [ $CORE_LOCAL_VERSION = $CORE_NPM_VERSION ]; then
   echo "Latest npm version is equal to current package version. Up the version to publish to npm."
 else
-  npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
   npm install
   npm run build
+  npm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN}
   npm publish --verbose --access=public
 fi
