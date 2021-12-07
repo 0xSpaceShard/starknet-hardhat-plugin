f7d36de7e9d7d37f26cbfe5e257f3fcd079d5375
FabijanC <fabijan.corak@gmail.com>
2021-12-07 09:27:16 +0100

    Disable starknet-verify test

--- test/starknet-verify/check.sh
+++ test/starknet-verify/check.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-npx hardhat starknet-compile
-output=$(npx hardhat starknet-deploy --starknet-network alpha starknet-artifacts/contracts/test.cairo/)
-echo $output
-address=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
+echo "The starknet-verify test is temporarily disabled."
+echo "To enable it back, uncomment the lines in its check.sh."
+# npx hardhat starknet-compile
+# output=$(npx hardhat starknet-deploy --starknet-network alpha starknet-artifacts/contracts/test.cairo/)
+# echo $output
+# address=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
 
-echo "Address: $address"
+# echo "Address: $address"
 
-sleep 5m
+# sleep 5m
 
-npx hardhat starknet-verify --starknet-network alpha --path contracts/test.cairo --address $address
+# npx hardhat starknet-verify --starknet-network alpha --path contracts/test.cairo --address $address
