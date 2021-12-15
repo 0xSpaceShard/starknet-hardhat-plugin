5848a956339ea058952c640c98b522f5776c239c
dribeiro-ShardLabs <94369106+dribeiro-ShardLabs@users.noreply.github.com>
2021-12-15 16:13:01 +0000

    Fixed dockerOptions, changed tests to not use test.cairo file (#33)

--- test/general-tests/starknet-verify/check.sh
+++ test/general-tests/starknet-verify/check.sh
@@ -4,8 +4,8 @@ set -e
 echo "The starknet-verify test is temporarily disabled."
 echo "To enable it back, uncomment the lines in its check.sh."
 
-# npx hardhat starknet-compile
-# output=$(npx hardhat starknet-deploy --starknet-network "$NETWORK" starknet-artifacts/contracts/test.cairo/)
+# npx hardhat starknet-compile contracts/util.cairo
+# output=$(npx hardhat starknet-deploy --starknet-network "$NETWORK" starknet-artifacts/contracts/util.cairo/)
 # echo $output
 # address=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
 
@@ -13,4 +13,4 @@ echo "To enable it back, uncomment the lines in its check.sh."
 
 # sleep 5m
 
-# npx hardhat starknet-verify --starknet-network "$NETWORK" --path contracts/test.cairo --address $address
+# npx hardhat starknet-verify --starknet-network "$NETWORK" --path contracts/util.cairo --address $address
