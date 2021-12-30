ecc89df49ce6f605922901dd1d910c24c6eb39f0
FabijanC <fabijan.corak@gmail.com>
2021-12-30 17:26:29 +0100

    Finish testing

--- test/configuration-tests/with-networks/check.sh
+++ test/configuration-tests/with-networks/check.sh
@@ -2,35 +2,28 @@
 set -e
 
 npx hardhat starknet-compile contracts/contract.cairo
-artifact_path=starknet-artifacts/contracts/contract.cairo/
-
-actual_output=$(mktemp)
-
-total_tests=0
-successful_tests=0
+ARTIFACT_PATH=starknet-artifacts/contracts/contract.cairo/
+INVALID_NETWORK="foo"
+PREFIX=$(dirname "$0")
 
 echo "Testing no starknet network"
-npx hardhat starknet-deploy "$artifact_path" --inputs 10 | tail -n +2 > "$actual_output" 2>&1 || echo "Successfully failing"
-diff actual_output no-starknet-network.txt
+npx hardhat starknet-deploy "$ARTIFACT_PATH" --inputs 10 2>&1 \
+    | tail -n +2 \
+    | diff - "$PREFIX/without-starknet-network.txt"
+echo "Success"
 
 echo "Testing invalid starknet network"
-npx hardhat starknet-deploy --starknet-network foo "$artifact_path" --inputs 10 | tail -n +2 > "$actual_output" 2>&1 || echo "Successfully failing"
-diff "$actual_output" invalid-starknet-network.txt
-
-echo "Testing starknet network with no url"
-npx hardhat starknet-deploy --starknet-network bar "$artifact_path" --inputs 10 | tail -n +2 > "$actual_output" 2>&1 || echo "Successfully failing"
-diff "$actual_output" starknet-network-without-url.txt
+npx hardhat starknet-deploy --starknet-network "$INVALID_NETWORK" "$ARTIFACT_PATH" --inputs 10 2>&1 \
+    | tail -n +2 \
+    | diff - "$PREFIX/invalid-starknet-network.txt"
+echo "Success"
 
 echo "Testing no mocha network"
-NETWORK="" npx hardhat test test/quick-test.ts > "$actual_output" 2>&1 && echo "Succeeding regardless"
-diff "$actual_output" no-mocha-network.txt
+NETWORK=""    npx hardhat test test/contract-factory-test.ts
+echo "Success"
 
 echo "Testing invalid mocha network"
-NETWORK="foo" npx hardhat test test/quick-test.ts > "$actual_output" 2>&1 || echo "Successfully failing"
-diff "$actual_output" invalid-mocha-network.txt
-
-echo "Testing mocha network with no url"
-NETWORK="bar" npx hardhat test test/quick-test.ts > actual_putput 2>&1 || echo "Successfully failing"
-diff "$actual_output" mocha-network-with-no-url.txt
-
-rm "$actual_output"
+NETWORK="$INVALID_NETWORK" npx hardhat test test/contract-factory-test.ts 2>&1 \
+    | tail -n +2 \
+    | diff - "$PREFIX/invalid-mocha-network.txt"
+echo "Success"
--- test/configuration-tests/with-networks/hardhat.config.ts
+++ test/configuration-tests/with-networks/hardhat.config.ts
@@ -4,8 +4,7 @@ module.exports = {
     networks: {
         devnet: {
             url: "http://localhost:5000"
-        },
-        bar: "http://localhost:1234" // invalid because no url key
+        }
     },
     mocha: {
         starknetNetwork: process.env.NETWORK
