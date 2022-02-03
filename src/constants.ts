69f1f3da3bb1deed7118b9172ba1fc59be859af6
dribeiro-ShardLabs <94369106+dribeiro-ShardLabs@users.noreply.github.com>
2022-02-03 16:35:24 +0000

    Fixed UnhandledPromise error, retry check status on 503 (#52)
    * Fixed UnhandledPromise error, retry check status on 503

--- src/constants.ts
+++ src/constants.ts
@@ -16,6 +16,8 @@ export const ALPHA_URL = "https://alpha4.starknet.io";
 export const ALPHA_MAINNET_URL = "https://alpha-mainnet.starknet.io";
 
 export const CHECK_STATUS_TIMEOUT = 5000; // ms
+export const CHECK_STATUS_RECOVER_TIMEOUT = 10000; // ms
+
 export const LEN_SUFFIX = "_len";
 export const VOYAGER_GOERLI_CONTRACT_API_URL = "https://goerli.voyager.online/api/contract/";
 export const VOYAGER_MAINNET_CONTRACT_API_URL = "https://voyager.online/api/contract/";
