e99d0be89db30200f922f9d0df235273081676d3
FabijanC <fabijan.corak@gmail.com>
2022-02-15 09:47:51 +0100

    Fix poetry to v1.1.12 (#56)

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
+
 set -e
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python - --version 1.1.12
 
 git clone -b master --single-branch git@github.com:Shard-Labs/starknet-devnet.git
 
-source $HOME/.poetry/env
+source "$HOME/.poetry/env"
 
 cd starknet-devnet 
-
 poetry build
-
 pip3 install dist/starknet_devnet-*-py3-none-any.whl
-
 cd ..
