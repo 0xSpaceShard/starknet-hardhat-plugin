6ce97e54932c910fc2f30fbd764d6ef17a4b48af
Antun Badurina <badurina.antun@gmail.com>
2022-02-18 10:02:23 +0100

    Fix lint errors

--- src/devnet-utils.ts
+++ src/devnet-utils.ts
@@ -1,4 +1,3 @@
-
 import axios from "axios";
 import { HardhatPluginError } from "hardhat/plugins";
 import { Devnet, HardhatRuntimeEnvironment } from "hardhat/types";
@@ -7,56 +6,64 @@ import { PLUGIN_NAME } from "./constants";
 import { getNetwork } from "./utils";
 
 interface L2Message {
-  from_address: BigInt;
-  to_address: BigInt;
-  payload: Array<BigInt>;
+    from_address: BigInt;
+    to_address: BigInt;
+    payload: Array<BigInt>;
 }
 
 export interface FlushResponse {
-  l1_provider: string;
-  n_consumed_l2_to_l1_messages: number;
-  consumed_l2_messages: Array<L2Message>;
+    l1_provider: string;
+    n_consumed_l2_to_l1_messages: number;
+    consumed_l2_messages: Array<L2Message>;
 }
 
 export interface LoadL1MessagingContractResponse {
-  address: string;
-  l1_provider: string;
+    address: string;
+    l1_provider: string;
 }
 
 export class DevnetUtils implements Devnet {
-  constructor(private hre: HardhatRuntimeEnvironment) {}
+    constructor(private hre: HardhatRuntimeEnvironment) {}
 
-  private get endpoint() {
-    const network = getNetwork(this.hre.config.starknet.network, this.hre, 'starknet.network');
+    private get endpoint() {
+        const network = getNetwork(this.hre.config.starknet.network, this.hre, "starknet.network");
 
-    return `${network.url}/postman`
-  }
-
-  private handleError(error: unknown) {
-    const parent = error instanceof Error && error;
-
-    throw new HardhatPluginError(PLUGIN_NAME, 'Request failed. Make sure your network has the /postman endpoint', parent);
-  }
-
-  public async flush() {
-    try {
-      const response = await axios.post<FlushResponse>(`${this.endpoint}/flush`);
-      return response.data;
-    } catch (error) {
-      this.handleError(error);
+        return `${network.url}/postman`;
     }
-  }
 
-  public async loadL1MessagingContract(networkUrl: string, address?: string, networkId?: string) {
-    try {
-      const response =
-        await axios.post<LoadL1MessagingContractResponse>(`${this.endpoint}/load_l1_messaging_contract`, {
-          networkId, address, networkUrl
-        });
+    private handleError(error: unknown) {
+        const parent = error instanceof Error && error;
 
-      return response.data;
-    } catch (error) {
-      this.handleError(error);
+        throw new HardhatPluginError(
+            PLUGIN_NAME,
+            "Request failed. Make sure your network has the /postman endpoint",
+            parent
+        );
     }
-  }
+
+    public async flush() {
+        try {
+            const response = await axios.post<FlushResponse>(`${this.endpoint}/flush`);
+            return response.data;
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
+
+    public async loadL1MessagingContract(networkUrl: string, address?: string, networkId?: string) {
+        try {
+            const response = await axios.post<LoadL1MessagingContractResponse>(
+                `${this.endpoint}/load_l1_messaging_contract`,
+                {
+                    networkId,
+                    address,
+                    networkUrl
+                }
+            );
+
+            return response.data;
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
 }
--- test/general-tests/postman/hardhat.config.ts
+++ test/general-tests/postman/hardhat.config.ts
@@ -4,14 +4,13 @@ import "@nomiclabs/hardhat-ethers";
 import "../dist/index.js";
 
 module.exports = {
-  solidity: '0.6.12',
-  starknet: {
-    network: process.env.NETWORK
-  },
-  networks: {
-    devnet: {
-      url: "http://localhost:5000"
+    solidity: "0.6.12",
+    starknet: {
+        network: process.env.NETWORK
     },
-  },
+    networks: {
+        devnet: {
+            url: "http://localhost:5000"
+        }
+    }
 };
-
