d91844f55a9e251a49b3b440ffc5840fa82b8cc6
Diogo Ribeiro <dr@shardlabs.io>
2022-02-24 11:55:25 +0000

    Fixed Account contract path

--- test/general-tests/account-test/check.sh
+++ test/general-tests/account-test/check.sh
@@ -2,6 +2,6 @@
 set -e
 
 npx hardhat starknet-compile contracts/contract.cairo
-npx hardhat starknet-compile account-contracts/contracts/Account.cairo
+npx hardhat starknet-compile contracts/Account.cairo
 
 npx hardhat test --no-compile test/account-test.ts
