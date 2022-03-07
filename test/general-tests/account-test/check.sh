696078e7ab3a7957be0fdfd3cb209d9375c6df99
Diogo Ribeiro <dr@shardlabs.io>
2022-03-07 12:14:35 +0000

    Fixed test script

--- test/general-tests/account-test/check.sh
+++ test/general-tests/account-test/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-npx hardhat starknet-compile contracts/contract.cairo contracts/Account.cairo
+npx hardhat starknet-compile contracts/contract.cairo
 
 npx hardhat test --no-compile test/account-test.ts
