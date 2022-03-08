3bf90cb5e8c14fe195ff6b050324794bcecac5be
dribeiro-ShardLabs <94369106+dribeiro-ShardLabs@users.noreply.github.com>
2022-03-08 12:47:04 +0000

    Automatic account artifacts fetching (#72)
    * Added automatic artifact fetching
    
    * Fix according to PR review

---
+++ src/account-utils.ts
@@ -0,0 +1,127 @@
+import { Numeric } from "./types";
+import { hash } from "starknet";
+import { BigNumberish, toBN } from "starknet/utils/number";
+import * as ellipticCurve from "starknet/utils/ellipticCurve";
+import { ec } from "elliptic";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as fs from "fs";
+import path from "path";
+import {
+    ABI_SUFFIX,
+    ACCOUNT_ARTIFACTS_VERSION,
+    ACCOUNT_CONTRACT_ARTIFACTS_ROOT_PATH,
+    GITHUB_ACCOUNT_ARTIFACTS_URL
+} from "./constants";
+import axios from "axios";
+
+/*
+ * Helper cryptography functions for Key generation and message signing
+ */
+
+export function generateRandomStarkPrivateKey(length = 63) {
+    const characters = "0123456789ABCDEF";
+    let result = "";
+    for (let i = 0; i < length; ++i) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return toBN(result, "hex");
+}
+
+/**
+ * Returns a signature which is the result of signing a message
+ *
+ * @param keyPair
+ * @param accountAddress
+ * @param nonce
+ * @param functionSelector
+ * @param toAddress
+ * @param calldata
+ * @returns the signature
+ */
+export function sign(
+    keyPair: ec.KeyPair,
+    accountAddress: string,
+    nonce: BigNumberish,
+    functionSelector: string,
+    toAddress: string,
+    calldata: BigNumberish[]
+): Numeric[] {
+    const msgHash = hash.computeHashOnElements([
+        toBN(accountAddress.substring(2), "hex"),
+        toBN(toAddress.substring(2), "hex"),
+        functionSelector,
+        toBN(hash.computeHashOnElements(calldata).substring(2), "hex"),
+        nonce
+    ]);
+
+    const signedMessage = ellipticCurve.sign(keyPair, BigInt(msgHash).toString(16));
+    const signature = [
+        BigInt("0x" + signedMessage[0].toString(16)),
+        BigInt("0x" + signedMessage[1].toString(16))
+    ];
+    return signature;
+}
+
+export async function handleAccountContractArtifacts(
+    accountType: string,
+    artifactsName: string,
+    hre: HardhatRuntimeEnvironment
+) {
+    // Name of the artifacts' parent folder
+    const targetPath = artifactsName + ".cairo";
+
+    // Full path to where the artifacts will be saved
+    const artifactsTargetPath = path.join(
+        hre.config.paths.starknetArtifacts,
+        ACCOUNT_CONTRACT_ARTIFACTS_ROOT_PATH,
+        ACCOUNT_ARTIFACTS_VERSION,
+        targetPath
+    );
+
+    if (!fs.existsSync(artifactsTargetPath)) {
+
+        // Check if an old version of the artifacts still exists in the path, if so delete it
+        const baseArtifactsPath = path.join(
+            hre.config.paths.starknetArtifacts,
+            ACCOUNT_CONTRACT_ARTIFACTS_ROOT_PATH
+        );
+
+        const contents = fs.readdirSync(baseArtifactsPath);
+
+        if (!(ACCOUNT_ARTIFACTS_VERSION in contents)) {
+            contents.forEach((content) => {
+                fs.rmSync(path.join(baseArtifactsPath, content), { recursive: true, force: true });
+            });
+        }
+
+        const jsonArtifact = artifactsName + ".json";
+        const abiArtifact = artifactsName + ABI_SUFFIX;
+
+        fs.mkdirSync(artifactsTargetPath, { recursive: true });
+
+        const fileLocationUrl = GITHUB_ACCOUNT_ARTIFACTS_URL.concat(
+            accountType,
+            "/",
+            targetPath,
+            "/"
+        );
+
+        await downloadArtifact(jsonArtifact, artifactsTargetPath, fileLocationUrl);
+        await downloadArtifact(abiArtifact, artifactsTargetPath, fileLocationUrl);
+    }
+}
+async function downloadArtifact(
+    artifactName: string,
+    artifactsTargetPath: string,
+    fileLocationUrl: string
+) {
+    const rawFileURL = fileLocationUrl.concat(artifactName);
+    const response = await axios.get(rawFileURL, {
+        transformResponse: (res) => {
+            return res;
+        },
+        responseType: "json"
+    });
+
+    fs.writeFileSync(path.join(artifactsTargetPath, artifactName), response.data);
+}
--- test/general-tests/account-test/check.sh
+++ test/general-tests/account-test/check.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
-npx hardhat starknet-compile contracts/contract.cairo contracts/Account.cairo
+DUMMY_DIR=starknet-artifacts/account-contract-artifacts/0.0.0/Account.cairo
+
+mkdir -p "$DUMMY_DIR"
+
+npx hardhat starknet-compile contracts/contract.cairo
 
 npx hardhat test --no-compile test/account-test.ts
+
+echo "Testing removal of dummy directory"
+if [ -d "$DUMMY_DIR" ]; then
+    # If path exists, exit with an error because it should have been deleted while fetching the correct artifacts
+    exit 1
+fi
+echo "Success"
