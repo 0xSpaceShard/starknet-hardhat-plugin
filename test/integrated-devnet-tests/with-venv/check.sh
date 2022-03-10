01b95599a139604888e67877cb6a275db0ca7990
Antun Badurina <antun.badurina@gmail.com>
2022-03-10 11:04:35 +0100

    Remove ls commands

--- test/integrated-devnet-tests/with-venv/check.sh
+++ test/integrated-devnet-tests/with-venv/check.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-ls
-ls ..
-ls ../..
-../..scripts/check-devnet-is-not_running.sh
+../../scripts/check-devnet-is-not-running.sh
 
 check_devnet_is_not_running
 npx hardhat starknet-compile contracts/contract.cairo
