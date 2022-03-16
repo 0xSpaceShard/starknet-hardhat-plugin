ad3b0152e7e6294942972dcac64dfdcbb37fe10e
FabijanC <fabijan.corak@gmail.com>
2022-03-16 17:13:04 +0100

    Use python 3.8.9

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python - --version 1.1.12
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    which /opt/circleci/.pyenv/versions/3.8.9/bin/python3.8 || pyenv install 3.8.9
+    pyenv global 3.8.9
+fi
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.12
 
 git clone -b master --single-branch git@github.com:Shard-Labs/starknet-devnet.git
 
 source "$HOME/.poetry/env"
 
 cd starknet-devnet 
+poetry env use "$(which python)"
 poetry build
 pip3 install dist/starknet_devnet-*-py3-none-any.whl
 cd ..
