8bbeeb6849d5d74b61f0d5b25277a7a09714cfdf
FabijanC <fabijan.corak@gmail.com>
2022-03-29 14:21:23 +0200

    Add assert-contains.py script

--- test/configuration-tests/with-compilation-options/check.sh
+++ test/configuration-tests/with-compilation-options/check.sh
@@ -7,11 +7,11 @@ CONTRACT_NAME=contract_with_execute.cairo
 
 cp "$PREFIX/$CONTRACT_NAME" contracts/
 
+EXPECTED='Only account contracts may have a function named "__execute__". Use --account-contract flag.'
+
 echo "Testing rejection of compilation without the account flag"
 npx hardhat starknet-compile "contracts/$CONTRACT_NAME" 2>&1 \
-    | tail -n +3 \
-    | head -n 1 \
-    | diff - "$PREFIX/without-account-flag.txt"
+    | ../scripts/assert-contains.py "$EXPECTED"
 echo "Success"
 
 npx hardhat starknet-compile "contracts/$CONTRACT_NAME" --account-contract
