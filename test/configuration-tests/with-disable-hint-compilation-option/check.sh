86f382532193df7ae9913d3ff7130cc7b7221b3a
LucasLvy <lucaslevy10@gmail.com>
2022-03-29 16:31:58 +0200

    feat(test): refacto disable hint validation test with py script

--- test/configuration-tests/with-disable-hint-compilation-option/check.sh
+++ test/configuration-tests/with-disable-hint-compilation-option/check.sh
@@ -7,13 +7,12 @@ CONTRACT_NAME=contract_with_unwhitelisted_hints.cairo
 
 cp "$PREFIX/$CONTRACT_NAME" contracts/
 
-echo "Testing rejection of compilation without the account flag"
-
-ERROR_MSG="Hint is not whitelisted.
+EXPECTED="Hint is not whitelisted.
 This may indicate that this library function cannot be used in StarkNet contracts."
 
-npx hardhat starknet-compile "$CONTRACT_NAME" 2>&1 | pcregrep -M -q "$ERROR_MSG"
-
+echo "Testing rejection of compilation without the --disable-hint-validation flag"
+npx hardhat starknet-compile "$CONTRACT_NAME" 2>&1 |
+    ../scripts/assert-contains.py "$EXPECTED"
 echo "Success"
 
 npx hardhat starknet-compile "$CONTRACT_NAME" --disable-hint-validation
