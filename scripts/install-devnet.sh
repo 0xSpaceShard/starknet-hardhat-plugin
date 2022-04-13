fe51ec2d20674d3996bad061f5ce4e44ba5917f1
Antun Badurina <antun.badurina@gmail.com>
2022-04-13 09:40:25 +0200

    Revert to master devnet install

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -4,6 +4,14 @@ set -e
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.12
 
+git clone -b master --single-branch git@github.com:Shard-Labs/starknet-devnet.git
+
 source "$HOME/.poetry/env"
 
-pip3 install starknet-devnet==0.1.20
+cd starknet-devnet
+poetry env use "$(which python3)"
+poetry build
+pip3 install dist/starknet_devnet-*-py3-none-any.whl
+cd ..
+
+# pip3 install starknet-devnet==0.1.20
