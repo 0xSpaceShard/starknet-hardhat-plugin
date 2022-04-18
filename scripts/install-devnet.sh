231376bc026a791a2a23c5888bab64b3377ee36d
FabijanC <fabijan.corak@gmail.com>
2022-04-18 13:31:35 +0200

    Use devnet 0.1.21

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.12
-
-git clone -b master --single-branch git@github.com:Shard-Labs/starknet-devnet.git
-
-source "$HOME/.poetry/env"
-
-cd starknet-devnet 
-poetry env use "$(which python3)"
-poetry build
-pip3 install dist/starknet_devnet-*-py3-none-any.whl
-cd ..
+pip3 install starknet-devnet==0.1.21
