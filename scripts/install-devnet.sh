8feead6a08a89cbb21f1a4fe33eacf1691e7221b
Antun Badurina <antun.badurina@gmail.com>
2022-04-19 15:33:59 +0200

    Install devnet from call-contract branch

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -2,4 +2,16 @@
 
 set -e
 
-pip3 install starknet-devnet==0.1.21
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 - --version 1.1.12
+
+git clone -b contract-call --single-branch git@github.com:Shard-Labs/starknet-devnet.git
+
+source "$HOME/.poetry/env"
+
+cd starknet-devnet
+poetry env use "$(which python3)"
+poetry build
+pip3 install dist/starknet_devnet-*-py3-none-any.whl
+cd ..
+
+# pip3 install starknet-devnet==0.1.21
