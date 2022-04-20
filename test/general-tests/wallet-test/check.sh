b8e9c7cded818fc0c7c60c202e3cbba11099d75c
Antun Badurina <antun.badurina@gmail.com>
2022-04-20 11:41:23 +0200

    Remove skip

--- test/general-tests/wallet-test/check.sh
+++ test/general-tests/wallet-test/check.sh
@@ -5,5 +5,4 @@ npx hardhat starknet-compile contracts/contract.cairo
 output=$(npx hardhat starknet-deploy-account --starknet-network "$NETWORK" --wallet OpenZeppelin)
 echo $output
 
-echo "This is skipped because it doesn't work with the cairo version 0.8.1"
-# npx hardhat test --no-compile test/wallet-test.ts
+npx hardhat test --no-compile test/wallet-test.ts
