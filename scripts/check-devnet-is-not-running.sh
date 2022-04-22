3008eaacd1b7cca6b479971744558f391fd8cf96
Antun Badurina <antun.badurina@gmail.com>
2022-04-22 13:00:02 +0200

    Fix typos

--- scripts/check-devnet-is-not-running.sh
+++ scripts/check-devnet-is-not-running.sh
@@ -6,7 +6,7 @@ check_devnet_is_not_running() {
     status=$(curl -s -o /dev/null -w "%{http_code}" "http://localhost:5000/feeder_gateway/is_alive") || echo "Devnet is not running! $status"
 
     if [ "$status" != 000 ]; then
-        echo "Devnet is running and responed with $status status!"
+        echo "Devnet is running and responed with status $status"
         exit 1
     fi
 }
