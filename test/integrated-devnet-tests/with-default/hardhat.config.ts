fcf1864b14ea1ba96ba0083db44f82b4b594ac4e
Antun Badurina <antun.badurina@gmail.com>
2022-04-22 13:01:55 +0200

    Remove network definition

--- test/integrated-devnet-tests/with-default/hardhat.config.ts
+++ test/integrated-devnet-tests/with-default/hardhat.config.ts
@@ -3,10 +3,5 @@ import "../dist/index.js";
 module.exports = {
     starknet: {
         network: process.env.NETWORK
-    },
-    networks: {
-        integratedDevnet: {
-            url: "http://127.0.0.1:5000"
-        }
     }
 };
