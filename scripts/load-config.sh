922be9e720e8eb80319b1aa307dbce7adc14edbf
FabijanC <fabijan.corak@gmail.com>
2022-05-03 15:34:58 +0200

    Add load-config logic to config.yml

--- scripts/load-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Iterates through config.json keys and set environment variables based on the key name
-
-for key in $(jq -r 'keys[]' config.json); do
-    export "$key"="$(jq -r .[\""$key"\"] config.json)"
-done
