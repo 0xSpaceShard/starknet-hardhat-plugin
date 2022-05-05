3631f04e0c6a0733b38613f2b380b8e09edf9f68
FabijanC <fabijan.corak@gmail.com>
2022-05-05 15:43:00 +0200

    Fix voyager verification

--- test/general-tests/starknet-verify/check.sh
+++ test/general-tests/starknet-verify/check.sh
@@ -11,18 +11,15 @@ output=$(npx hardhat starknet-deploy --starknet-network "$NETWORK" contract --in
 address=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
 
 echo "Sleeping to allow Voyager to index the deployment"
-sleep 30s
+sleep 1m
 
 echo "Verifying contract at $address"
 npx hardhat starknet-verify --starknet-network "$NETWORK" --path $MAIN_CONTRACT $UTIL_CONTRACT --address $address
 
-echo "Sleeping before fetching the verified code - seems to be necessary"
-sleep 30s
-
 is_verified=$(curl "https://goerli.voyager.online/api/contract/$address/code" | jq ".abiVerified")
-if [ is_verified == "true" ]; then
+if [ "$is_verified" == "true" ]; then
     echo "Successfully verified!"
 else
-    echo "Error: Not verified!"
+    echo "$0: Error: Not verified!"
     exit 1
 fi
