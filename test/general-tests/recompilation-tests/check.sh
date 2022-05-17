e35c7091ee4a16af544f2e8c6c8a9f50d5435684
Nathan-SL <nl@shardlabs.io>
2022-05-17 13:05:15 +0300

    Remove echo for success after hardhat test

--- test/general-tests/recompilation-tests/check.sh
+++ test/general-tests/recompilation-tests/check.sh
@@ -8,27 +8,22 @@ cat dependency.cairo >> contracts/dependency.cairo
 echo "Testing Recompilation with new contract added"
 cat hello.cairo >> contracts/hello.cairo
 npx hardhat test test/recompilation/recompilation-main-test.ts
-echo "Success"
 
 echo "Testing Recompilation with artifacts deleted"
 rm -rf starknet-artifacts/contracts/contract.cairo
 npx hardhat test test/recompilation/recompilation-main-test.ts
-echo "Success"
 
 echo "Testing Recompilation with updated contract"
 cat get_balance.cairo >> contracts/hello.cairo
 npx hardhat test test/recompilation/recompilation-update-test.ts
-echo "Success"
 
 echo "Testing Recompilation with cache file deleted"
 rm -rf cache/cairo-files-cache.json
 npx hardhat test test/recompilation/recompilation-main-test.ts
-echo "Success"
 
 echo "Testing Recompilation with dependency changed"
 echo "#" >> contracts/dependency.cairo
 npx hardhat test test/recompilation/recompilation-dependency-test.ts
-echo "Success"
 
 echo "Testing Recompilation one file added another deleted"
 rm -f contracts/hello.cairo
@@ -36,4 +31,3 @@ rm -f contracts/hello.cairo
 cat hello.cairo >> contracts/hello.cairo
 rm -f contracts/dependency.cairo
 npx hardhat test test/recompilation/recompilation-main-test.ts
-echo "Success"
