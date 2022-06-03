4a768f95a2d4803c2ca231c024c5ebfa2df42c6e
Nathan-SL <nl@shardlabs.io>
2022-06-03 13:30:43 +0300

    Add test case for recompile opiton on default
    - change diff to assert-contains.py for stdin pattern

--- test/recompilation-tests/recompiler-default/check.sh
+++ test/recompilation-tests/recompiler-default/check.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+EXPECTED='HardhatPluginError: Could not find metadata for contract "contract.cairo"'
 
-echo "Testing with deleted artifact on recompiler option set to false"
+echo "Testing with deleted artifact on recompiler option set to default (off)"
 rm -rf starknet-artifacts/contracts/contract.cairo/
 npx hardhat run --no-compile scripts/deploy.ts 2>&1 |
     ../scripts/assert-contains.py "$EXPECTED"
