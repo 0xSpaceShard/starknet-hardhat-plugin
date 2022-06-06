fc22bbebe2ca6e3259065e4a05cbedb5a58a4a4c
Nathan-SL <nl@shardlabs.io>
2022-06-06 11:19:05 +0300

    Handle cache on starknet-call

--- src/task-actions.ts
+++ src/task-actions.ts
@@ -398,6 +398,7 @@ export async function starknetInvokeAction(args: TaskArguments, hre: HardhatRunt
 }
 
 export async function starknetCallAction(args: TaskArguments, hre: HardhatRuntimeEnvironment) {
+    await handleCache(hre);
     await starknetInteractAction(InteractChoice.CALL, args, hre);
 }
 
