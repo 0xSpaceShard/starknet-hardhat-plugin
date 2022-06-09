72eaf44619e1e9502b8d050f893f85e0e9898bb4
FabijanC <fabijan.corak@gmail.com>
2022-06-09 18:18:12 +0200

    Sleep after starknet-verify [skip ci]

--- test/general-tests/starknet-verify/check.sh
+++ test/general-tests/starknet-verify/check.sh
@@ -11,13 +11,14 @@ npx hardhat starknet-compile $MAIN_CONTRACT $UTIL_CONTRACT
 echo "Waiting for deployment to be accepted"
 output=$(npx hardhat starknet-deploy --starknet-network "$NETWORK" contract --inputs 10 --wait)
 address=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
+echo "Verifying contract at $address"
 
 echo "Sleeping to allow Voyager to index the deployment"
 sleep 1m
 
-echo "Verifying contract at $address"
-
 npx hardhat starknet-verify --starknet-network "$NETWORK" --path $MAIN_CONTRACT $UTIL_CONTRACT --address $address --compiler-version 0.8.1 --license "No License (None)"
+echo "Sleeping to allow Voyager to register the verification"
+sleep 15s
 
 is_verified=$(curl "https://goerli.voyager.online/api/contract/$address/code" | jq ".abiVerified")
 if [ "$is_verified" == "true" ]; then
