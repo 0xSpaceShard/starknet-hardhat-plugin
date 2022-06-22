d51673fa1db892ae1854693bd8c8c380a70bad1d
Nathan-SL <nl@shardlabs.io>
2022-06-22 16:24:30 +0300

    Add integratedDevnet url and dockerVersion

--- test/recompilation-tests/starknet-deploy/hardhat.config.ts
+++ test/recompilation-tests/starknet-deploy/hardhat.config.ts
@@ -5,5 +5,11 @@ module.exports = {
     starknet: {
         network: process.env.NETWORK,
         recompile: true
+    },
+    networks: {
+        integratedDevnet: {
+            dockerizedVersion: process.env.STARKNET_DEVNET,
+            url: "http://127.0.0.1:5050"
+        }
     }
 };
