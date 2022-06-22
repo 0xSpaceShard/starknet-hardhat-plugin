3bec8a371efd7e0118bb50adf6c5ce6f1c1d2532
FabijanC <fabijan.corak@gmail.com>
2022-06-22 21:43:45 +0200

    Add --no-compile flag to running of argent test

--- test/general-tests/account-test/check.sh
+++ test/general-tests/account-test/check.sh
@@ -4,7 +4,7 @@ set -eu
 npx hardhat starknet-compile contracts/contract.cairo contracts/util.cairo
 
 if [ "$NETWORK" == "devnet" ]; then
-    npx hardhat test scripts/deploy-argent.ts
+    npx hardhat test --no-compile scripts/deploy-argent.ts
 fi
 
 npx hardhat test --no-compile test/oz-account-test.ts test/argent-account-test.ts
