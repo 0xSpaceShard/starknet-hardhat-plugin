c64dc24edc99cb95653a739fb63a672ef7891f6c
FabijanC <fabijan.corak@gmail.com>
2022-06-22 18:03:58 +0200

    Deploy argent account before account test

--- test/general-tests/account-test/check.sh
+++ test/general-tests/account-test/check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -e
+set -eu
 
 npx hardhat starknet-compile contracts/contract.cairo contracts/util.cairo
+
+if [ "$NETWORK" == "devnet" ]; then
+    npx hardhat test scripts/deploy-argent.ts
+fi
 
 npx hardhat test --no-compile test/oz-account-test.ts test/argent-account-test.ts
