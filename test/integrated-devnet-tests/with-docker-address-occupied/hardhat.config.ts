2c986d09aec64633f0c93c28932e9f23f7fad7b0
FabijanC <fabijan.corak@gmail.com>
2022-07-07 09:32:35 +0200

    Fix exiting in integrated-devnet (#132) [skip ci]
    Co-authored-by: Jorma <RedFox20@users.noreply.github.com>
    Co-authored-by: RedFox <jorma@tempus.finance>

---
+++ test/integrated-devnet-tests/with-docker-address-occupied/hardhat.config.ts
@@ -0,0 +1,13 @@
+import "../dist/src/index.js";
+
+module.exports = {
+    starknet: {
+        network: process.env.NETWORK
+    },
+    networks: {
+        integratedDevnet: {
+            dockerizedVersion: process.env.STARKNET_DEVNET,
+            url: "http://127.0.0.1:5050"
+        }
+    }
+};
