823f428c2999423e3223052adfcc59f99123e4a7
Alex Manuskin <alex@manuskin.org>
2022-07-17 12:14:27 +0300

    Update OpenZeppelin version to 0.2.1
    Updates OpenZeppelin account version to 0.2.1
    Fixes issue #161

--- src/account.ts
+++ src/account.ts
@@ -295,7 +295,7 @@ export abstract class Account {
 export class OpenZeppelinAccount extends Account {
     static readonly ACCOUNT_TYPE_NAME = "OpenZeppelinAccount";
     static readonly ACCOUNT_ARTIFACTS_NAME = "Account";
-    static readonly VERSION = "0.2.0";
+    static readonly VERSION = "0.2.1";
 
     constructor(
         starknetContract: StarknetContract,
