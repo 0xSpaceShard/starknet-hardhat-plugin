ee2c773a0445c63c82ab06eb0af61ef1d8ccb08b
Nathan-SL <nl@shardlabs.io>
2022-08-10 11:54:14 +0300

    Update test

--- test/general-tests/starknet-plugin-version/check.sh
+++ test/general-tests/starknet-plugin-version/check.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Check if plugin version returns the correct version
-PLUGIN_VERSION=$(jq -r ".version" package.json)
+PLUGIN_VERSION=$(npm view @shardlabs/starknet-hardhat-plugin dist-tags.latest)
 echo "Plugin version: $PLUGIN_VERSION"
 npx hardhat starknet-plugin-version 2>&1 |
     ../scripts/assert-contains.py "Version: $PLUGIN_VERSION"
