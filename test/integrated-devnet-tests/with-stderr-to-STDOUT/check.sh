de5671514cf57b47c6b854c1211f4e796220d488
Nathan-SL <nl@shardlabs.io>
2022-08-16 20:31:41 +0300

    Adapt expected error message to devnet 0.2.10

--- test/integrated-devnet-tests/with-stderr-to-STDOUT/check.sh
+++ test/integrated-devnet-tests/with-stderr-to-STDOUT/check.sh
@@ -9,4 +9,4 @@ check_devnet_is_not_running
 npx hardhat starknet-compile contracts/contract.cairo
 
 npx hardhat test --no-compile test/integrated-devnet.test.ts 2>&1 | 
-    ../scripts/assert-contains.py "starknet-devnet: error: argument --accounts: invalid int value: 'invalid_value'"
+    ../scripts/assert-contains.py "starknet-devnet: error: --accounts must be a positive integer; got: invalid_value."
