27bbdcd07ce193d24e1358df46e31e4cb055699f
FabijanC <fabijan.corak@gmail.com>
2022-08-19 15:19:19 +0200

    Apply formatting

--- src/external-server/docker-devnet.ts
+++ src/external-server/docker-devnet.ts
@@ -2,7 +2,14 @@ import { Image } from "@nomiclabs/hardhat-docker";
 import { DockerServer } from "./docker-server";
 
 export class DockerDevnet extends DockerServer {
-    constructor(image: Image, host: string, port: string, private devnetArgs?: string[], stdout?: string, stderr?: string) {
+    constructor(
+        image: Image,
+        host: string,
+        port: string,
+        private devnetArgs?: string[],
+        stdout?: string,
+        stderr?: string
+    ) {
         super(image, host, port, "is_alive", "integrated-devnet", devnetArgs, stdout, stderr);
     }
 
