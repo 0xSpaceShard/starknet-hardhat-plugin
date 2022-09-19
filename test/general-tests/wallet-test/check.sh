8b70aceb4e6e796f110cde7a232527fec0bafb0e
FabijanC <fabijan.corak@gmail.com>
2022-09-19 17:39:30 +0200

    Fund wallet deployed with starknet CLI

--- test/general-tests/wallet-test/check.sh
+++ test/general-tests/wallet-test/check.sh
@@ -12,5 +12,11 @@ echo $output
 
 ADDRESS=$(echo $output | sed -r "s/.*Contract address: (\w*).*/\1/")
 
+ACCOUNT_FILE="~/.starknet_accounts"
+ACCOUNT_ADDRESS=$(jq -r '.["alpha-goerli"].__default__.address' $ACCOUNT_FILE)
+curl 127.0.0.1:5050/mint \
+    -H "Content-Type: application/json" \
+    -d "{ \"address\": \"$ACCOUNT_ADDRESS\", \"amount\": 1000000000000000000, \"lite\": true }"
+
 npx hardhat starknet-call --contract contract --function get_balance --address "$ADDRESS" --wallet OpenZeppelin --starknet-network "$NETWORK"
 npx hardhat starknet-invoke --contract contract --function increase_balance --inputs "10 20" --address "$ADDRESS" --wallet OpenZeppelin --starknet-network "$NETWORK"
