6e32b10a30c9214dfd60524d0f714febfed26505
FabijanC <fabijan.corak@gmail.com>
2022-09-20 12:02:55 +0200

    Fix integrated devnet test with default runner

--- test/integrated-devnet-tests/with-default/hardhat.config.ts
+++ test/integrated-devnet-tests/with-default/hardhat.config.ts
@@ -1,9 +1,12 @@
 import "../dist/src/index.js";
 
+// defualt in the sense that neither venv nor dockerized devnet is specified
+
 module.exports = {
     starknet: {
         network: process.env.NETWORK,
         integratedDevnet: {
+            url: "http://127.0.0.1/5050",
             args: ["--seed", "42"]
         }
     }
