2f7e67f52a04f7c01f0f1c07f2d9af17b477e849
Nathan-SL <nl@shardlabs.io>
2022-09-30 17:31:31 +0300

    Update test
    - Change contract path

--- test/general-tests/cairo-migrate/check.sh
+++ test/general-tests/cairo-migrate/check.sh
@@ -2,15 +2,18 @@
 set -e
 
 CONTRACT_NAME=old_contract.cairo
+CONTRACT_PATH="contracts/$CONTRACT_NAME"
 NEW_COMMENT="// Declare this file as a StarkNet contract."
+
+cp "$(dirname "$0")/$CONTRACT_NAME" "$CONTRACT_PATH"
 
 echo "Testing migration of old cairo contract to a new one"
 # Migrate contract to new version.
-npx hardhat migrate "$CONTRACT_NAME" 2>&1 |
+npx hardhat migrate "$CONTRACT_PATH" 2>&1 |
     ../scripts/assert-contains.py "$NEW_COMMENT"
 
 # Migrate contract to new version with change content in place option.
-npx hardhat migrate "$CONTRACT_NAME" --inplace
-cat "$CONTRACT_NAME" | ../scripts/assert-contains.py "$NEW_COMMENT"
+npx hardhat migrate "$CONTRACT_PATH" --inplace
+cat "$CONTRACT_PATH" | ../scripts/assert-contains.py "$NEW_COMMENT"
 
 echo "Success"
