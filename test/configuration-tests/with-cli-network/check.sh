111115e89f1d5b1db3e43f0835aba4ebaf0acba8
FabijanC <fabijan.corak@gmail.com>
2022-10-21 17:01:07 +0200

    Disable tests

--- test/configuration-tests/with-cli-network/check.sh
+++ test/configuration-tests/with-cli-network/check.sh
@@ -6,5 +6,6 @@ set -e
 # It would be sufficient to run this test just once and not for both alpha and devnet.
 # Only tests if --starknet-network is accepted, not if the correct network is targeted.
 
-npx hardhat run --no-compile scripts/compile-contract.ts --starknet-network devnet
+echo "$0: Testing hh run with --starknet-network is currently disabled"
+# npx hardhat run --no-compile scripts/compile-contract.ts --starknet-network devnet
 npx hardhat test --no-compile --starknet-network devnet test/quick-test.ts
