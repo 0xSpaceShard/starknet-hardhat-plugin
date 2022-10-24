1103f873e504d9c4438fb9f604a3929ff1e94f00
FabijanC <fabijan.corak@gmail.com>
2022-10-24 09:53:09 +0200

    Fix test suppression

--- test/configuration-tests/with-cli-network/check.sh
+++ test/configuration-tests/with-cli-network/check.sh
@@ -6,6 +6,5 @@ set -e
 # It would be sufficient to run this test just once and not for both alpha and devnet.
 # Only tests if --starknet-network is accepted, not if the correct network is targeted.
 
-echo "$0: Testing hh run with --starknet-network is currently disabled"
-# npx hardhat run --no-compile scripts/compile-contract.ts --starknet-network devnet
+npx hardhat run --no-compile scripts/compile-contract.ts --starknet-network devnet
 npx hardhat test --no-compile --starknet-network devnet test/quick-test.ts
