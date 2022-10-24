e5296ea18340b740595d378a180405f34ab14697
FabijanC <fabijan.corak@gmail.com>
2022-10-24 10:40:45 +0200

    Throw if --starknet-network provided with hh run (#241)
    * [skip ci]

--- test/general-tests/hardhat-run/check.sh
+++ test/general-tests/hardhat-run/check.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-npx hardhat starknet-compile contracts/contract.cairo
-npx hardhat run --no-compile scripts/quick-script.ts --starknet-network "$NETWORK"
+echo "$0: Testing hh run with --starknet-network is currently disabled"
+
+# npx hardhat starknet-compile contracts/contract.cairo
+# npx hardhat run --no-compile scripts/quick-script.ts --starknet-network "$NETWORK"
