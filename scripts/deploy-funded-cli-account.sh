5bda442521320d6722b0b36c5936904056aaa818
Nathan <102951164+Nathan-SL@users.noreply.github.com>
2022-10-31 16:47:29 +0300

    starknet-new-account CLI command (#246)
    * [skip ci]

--- scripts/deploy-funded-cli-account.sh
+++ scripts/deploy-funded-cli-account.sh
@@ -9,7 +9,7 @@ fi
 
 ACCOUNT_FILE="$ACCOUNT_DIR/starknet_open_zeppelin_accounts.json"
 
-npx hardhat starknet-deploy-account --starknet-network "$NETWORK" --wallet OpenZeppelin
+npx hardhat starknet-new-account --starknet-network "$NETWORK" --wallet OpenZeppelin
 ACCOUNT_ADDRESS=$(jq -r .$NETWORK.OpenZeppelin.address $ACCOUNT_FILE)
 
 echo "Funding $ACCOUNT_ADDRESS"
@@ -17,3 +17,5 @@ echo "Funding $ACCOUNT_ADDRESS"
 curl 127.0.0.1:5050/mint \
     -H "Content-Type: application/json" \
     -d "{ \"address\": \"$ACCOUNT_ADDRESS\", \"amount\": 1000000000000000000, \"lite\": true }"
+
+npx hardhat starknet-deploy-account --starknet-network "$NETWORK" --wallet OpenZeppelin
