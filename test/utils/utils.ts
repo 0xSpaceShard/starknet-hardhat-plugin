aa04ee4fa1a8d1345d89c467599009e1953400bb
Nathan-SL <nl@shardlabs.io>
2022-11-22 16:08:35 +0300

    Update utils
    - Remove shell.exit, shell.echo
    - Use axios inplace of curl
    - Update contains function

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -1,31 +1,23 @@
+import axios from "axios";
+import { rmSync } from "fs";
 import shell from "shelljs";
+import { StarknetPluginError } from "../../src/starknet-plugin-error";
 
 export function exec(cmd: string) {
     const result = shell.exec(cmd);
     if (result.code !== 0) {
-        shell.exit(1);
+        throw new StarknetPluginError(`Command ${cmd} failed.\n${result.stderr}`);
     }
 
     return result;
 }
 
-export function contains(cmd: string, pattern: string, responseType?: string) {
-    if (responseType === "stdout") {
-        const res = shell.exec(cmd, { silent: true }).stdout;
-        if (!res.includes(pattern)) {
-            console.error("Pattern not in input");
-            console.error("Pattern:", pattern);
-            console.error("Input:", res);
-            shell.exit(1);
-        }
-        return;
-    }
-    const res = shell.exec(cmd, { silent: true }).stderr;
-    if (!res.includes(pattern)) {
+export function contains(output: string, pattern: string) {
+    if (!output.includes(pattern)) {
         console.error("Pattern not in input");
         console.error("Pattern:", pattern);
-        console.error("Input:", res);
-        shell.exit(1);
+        console.error("Input:", output);
+        throw new StarknetPluginError("Pattern not in input");
     }
 }
 
@@ -37,14 +29,26 @@ export function extractAddress(source: string, pattern: string) {
     return res;
 }
 
-export function checkDevnetIsNotRunning(): void {
-    const result = shell.exec("curl -s -o /dev/null -w \"%{http_code}\" http://127.0.0.1:5050/feeder_gateway/is_alive", { silent: true });
-    if (result.code !== 0) {
-        shell.echo("Devnet is not running!", result);
+export async function checkDevnetIsNotRunning(url?: string): Promise<void> {
+    url = url || "http://127.0.0.1:5050";
+    try {
+        const res = await axios.get(`${url}/is_alive`);
+        console.log(`Devnet is running and responded with status ${res.status}`);
+        throw new StarknetPluginError(`Devnet is running and responded with status ${res.status}`);
+    } catch (err) {
+        if (err.toJSON().code == "ECONNREFUSED") {
+            console.log("Devnet is not running!");
+        }
     }
+}
 
-    if (result.stdout !== "000") {
-        shell.echo("Devnet is running and responded with status", result);
-        shell.exit(1);
+export function ensureEnvVar(varName: string): string {
+    if (!process.env[varName]) {
+        throw new Error(`Env var ${varName} not set or empty`);
     }
+    return process.env[varName] as string;
+}
+
+export function rmrfSync(path: string) {
+    rmSync(path, { recursive: true, force: true });
 }
