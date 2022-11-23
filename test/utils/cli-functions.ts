26185a09c15061834cf20326f1663933dd0a2d7e
Nathan-SL <nl@shardlabs.io>
2022-11-23 22:31:14 +0300

    Fix typo

--- test/utils/cli-functions.ts
+++ test/utils/cli-functions.ts
@@ -1,48 +1,48 @@
 import shell from "shelljs";
 
-const exec = (cmd: string, silet?: boolean) => {
-    silet = silet || false;
-    return shell.exec(cmd, { silent: silet });
+const exec = (cmd: string, silent?: boolean) => {
+    silent = silent || false;
+    return shell.exec(cmd, { silent: silent });
 };
 
-export const hardhatStarknetCompile = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-compile ${args.join(" ")}`, silet);
+export const hardhatStarknetCompile = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-compile ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetDeploy = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-deploy ${args.join(" ")}`, silet);
+export const hardhatStarknetDeploy = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-deploy ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetInvoke = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-invoke ${args.join(" ")}`, silet);
+export const hardhatStarknetInvoke = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-invoke ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetCall = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-call ${args.join(" ")}`, silet);
+export const hardhatStarknetCall = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-call ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetEstimateFee = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-estimate-fee ${args.join(" ")}`, silet);
+export const hardhatStarknetEstimateFee = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-estimate-fee ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetNewAccount = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-new-account ${args.join(" ")}`, silet);
+export const hardhatStarknetNewAccount = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-new-account ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetDeployAccount = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat starknet-deploy-account ${args.join(" ")}`, silet);
+export const hardhatStarknetDeployAccount = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat starknet-deploy-account ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetRun = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat run ${args.join(" ")}`, silet);
+export const hardhatStarknetRun = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat run ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetTest = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat test ${args.join(" ")}`, silet);
+export const hardhatStarknetTest = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat test ${args.join(" ")}`, silent);
 };
 
-export const hardhatStarknetMigrate = (args: Array<string>, silet?: boolean) => {
-    return exec(`npx hardhat migrate ${args.join(" ")}`, silet);
+export const hardhatStarknetMigrate = (args: Array<string>, silent?: boolean) => {
+    return exec(`npx hardhat migrate ${args.join(" ")}`, silent);
 };
 
 export const hardhatStarknetPluginVersion = () => {
