fb0c77a8948621b6eb2c60c6a5b546d431c2a850
Nathan-SL <nl@shardlabs.io>
2022-11-24 11:37:26 +0300

    Read port from constant

--- test/general-tests/postman/check.ts
+++ test/general-tests/postman/check.ts
@@ -1,11 +1,12 @@
 import { exec } from "../../utils/utils";
 import { spawn } from "child_process";
 import { hardhatStarknetCompile, hardhatStarknetTest } from "../../utils/cli-functions";
+import { NODE_PORT } from "../../constants/constants";
 
 hardhatStarknetCompile(["contracts/l1l2.cairo"]);
 
-spawn("npx", ["hardhat", "node"], { detached: true });
+spawn("npx", ["hardhat", "node", "--port", NODE_PORT], { detached: true });
 exec("sleep 1");
 
 hardhatStarknetTest("--network localhost test/postman.test.ts".split(" "));
-exec("kill -9 $(lsof -t -i:8545)");
+exec(`kill -9 $(lsof -t -i:${NODE_PORT})`);
