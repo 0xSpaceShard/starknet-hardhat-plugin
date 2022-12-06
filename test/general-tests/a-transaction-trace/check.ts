2476097160e3fb390caeab29b145cd2cda35ea51
Nathan-SL <nl@shardlabs.io>
2022-12-06 14:54:58 +0300

    Update test

--- test/general-tests/a-transaction-trace/check.ts
+++ test/general-tests/a-transaction-trace/check.ts
@@ -1,3 +1,4 @@
-import { hardhatStarknetTest } from "../../utils/cli-functions";
+import { hardhatStarknetCompile, hardhatStarknetTest } from "../../utils/cli-functions";
 
+hardhatStarknetCompile(["contracts/contract.cairo"]);
 hardhatStarknetTest("--no-compile test/transaction-trace-test.ts".split(" "));
