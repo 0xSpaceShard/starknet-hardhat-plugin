f46fcee2951f83fe87a4db81f9b56023e073fd9e
FabijanC <fabijan.corak@gmail.com>
2022-12-12 15:16:02 +0100

    Update argent account test docs [skip ci]

--- test/general-tests/account-test/hardhat.config.ts
+++ test/general-tests/account-test/hardhat.config.ts
@@ -7,6 +7,8 @@ module.exports = {
     networks: {
         integratedDevnet: {
             url: "http://127.0.0.1:5050",
+            // testing with fork because alpha-goerli has the needed argent account contracts declared
+            // using integrated-devnet (in network.json) because spawning devnet is currently out of reach for invidiual tests
             args: ["--seed", "42", "--fork-network", "alpha-goerli"]
         }
     }
