854b434b0ee513540cccbb7616550f098d91030d
FabijanC <fabijan.corak@gmail.com>
2022-12-12 15:08:46 +0100

    Restore example repo branch [skip ci]

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,13 +9,13 @@ CONFIG_FILE_NAME="hardhat.config.ts"
 
 # setup example repo
 rm -rf starknet-hardhat-example
-EXAMPLE_REPO_BRANCH=adapt-0.10.2
+EXAMPLE_REPO_BRANCH="plugin"
 if [[ "$CIRCLE_BRANCH" == "master" ]] && [[ "$EXAMPLE_REPO_BRANCH" != "plugin" ]]; then
     echo "Invalid example repo branch: $EXAMPLE_REPO_BRANCH"
     exit 1
 fi
 
-git clone -b adapt-0.10.2 --single-branch git@github.com:Shard-Labs/starknet-hardhat-example.git
+git clone -b "$EXAMPLE_REPO_BRANCH" --single-branch git@github.com:Shard-Labs/starknet-hardhat-example.git
 cd starknet-hardhat-example
 git log -n 1
 npm ci
