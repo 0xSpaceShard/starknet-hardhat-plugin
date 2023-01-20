2a68757e30524e0191edc397e672e0bdff76c201
Nathan-SL <nl@shardlabs.io>
2023-01-20 18:07:19 +0300

    Update setup-venv.sh

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -13,10 +13,10 @@ echo "python version: $(python --version)"
 if [[ "$OSTYPE" == "darwin"* ]]; then
     export HOMEBREW_NO_INSTALL_CLEANUP=1
     brew ls --versions gmp || brew install gmp
+    CFLAGS=-I`brew --prefix gmp`/include LDFLAGS=-L`brew --prefix gmp`/lib pip3 install fastecdsa
 fi
 
 if [ "$TEST_SUBDIR" == "venv-tests" ]; then
-    CFLAGS=-I`brew --prefix gmp`/include LDFLAGS=-L`brew --prefix gmp`/lib pip3 install fastecdsa
     which "$VENV/bin/starknet" || pip3 install cairo-lang=="$(cat /tmp/cairo-lang-version)"
     echo "starknet at: $(which starknet)"
     echo "starknet version: $(starknet --version)"
