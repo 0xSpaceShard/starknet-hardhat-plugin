c7309c8b1d44116bdd263654019e225a95b0fbf0
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-23 14:33:06 +0800

    Image tags

--- scripts/docker-tester.sh
+++ scripts/docker-tester.sh
@@ -1,4 +1,4 @@
-docker build . -t starknet-hardhat-plugin-tester
+docker build . -t shard-labs/starknet-hardhat-tester:0.1.0
 docker run --rm -ti \
 -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/circleci/project \
---entrypoint bash starknet-hardhat-plugin-tester
+--entrypoint bash shard-labs/starknet-hardhat-tester:0.1.0
