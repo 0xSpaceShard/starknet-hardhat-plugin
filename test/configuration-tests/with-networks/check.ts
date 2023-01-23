49705406653259446b11aa3f268c241142cb0e9b
FabijanC <fabijan.corak@gmail.com>
2023-01-23 13:52:01 +0100

    Use contract-factory-creation.test.ts

--- test/configuration-tests/with-networks/check.ts
+++ test/configuration-tests/with-networks/check.ts
@@ -11,7 +11,12 @@ const prefix = __dirname;
 
 console.log("Testing invalid CLI network");
 let execution = hardhatStarknetTest(
-    ["test/contract-factory-test.ts", "--no-compile", "--starknet-network", invalidNetwork],
+    [
+        "test/contract-factory-creation.test.ts",
+        "--no-compile",
+        "--starknet-network",
+        invalidNetwork
+    ],
     true
 );
 assertContains(
@@ -22,12 +27,12 @@ console.log("Success");
 
 console.log("Testing no mocha network");
 process.env.NETWORK = "";
-hardhatStarknetTest(["--no-compile", "test/contract-factory-test.ts"]);
+hardhatStarknetTest(["--no-compile", "test/contract-factory-creation.test.ts"]);
 console.log("Success");
 
 console.log("Testing invalid config network");
 process.env.NETWORK = invalidNetwork;
-execution = hardhatStarknetTest(["--no-compile", "test/contract-factory-test.ts"], true);
+execution = hardhatStarknetTest(["--no-compile", "test/contract-factory-creation.test.ts"], true);
 assertContains(execution.stderr, expected);
 console.log("Success");
 
@@ -35,5 +40,5 @@ console.log("Testing deployment with alpha-goerli2 config network is temporarily
 // console.log("Testing with alpha-goerli2 config network");
 // process.env.NETWORK = "alpha-goerli2";
 // execution = hardhatStarknetTest(
-//    ["test/contract-factory-test.ts", "--no-compile", "--starknet-network", "alpha-goerli2"]
+//    ["test/contract-factory-creation.test.ts", "--no-compile", "--starknet-network", "alpha-goerli2"]
 // );
