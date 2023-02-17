802b8abffbe024871ebbd697f442e7e37b79eacf
notV4l <imV4l@proton.me>
2023-02-17 12:20:33 +0100

    missing tests [skip-testnet]

---
+++ test/integrated-devnet-tests/with-active-venv-rust/hardhat.config.ts
@@ -0,0 +1,15 @@
+import "@shardlabs/starknet-hardhat-plugin";
+
+module.exports = {
+    starknet: {
+        network: process.env.NETWORK
+    },
+    networks: {
+        integratedDevnet: {
+            venv: "active",
+            vmLang: "rust",
+            url: "http://127.0.0.1:5050",
+            args: ["--seed", "42"]
+        }
+    }
+};
