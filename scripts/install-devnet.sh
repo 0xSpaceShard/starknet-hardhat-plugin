844a04dd1beebf58372ed02bdf53bff00e39a295
notV4l <imV4l@proton.me>
2023-02-17 12:45:54 +0100

    add rust install [skip-testnet]

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -6,5 +6,10 @@ pip3 install "starknet-devnet==$STARKNET_DEVNET"
 STARKNET_DEVNET_PATH=$(which starknet-devnet)
 echo "starknet-devnet at: $STARKNET_DEVNET_PATH"
 
+echo "starting rust install"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "installed rust"
+
+echo "starting cairo-rs-py install"
 pip3 install cairo-rs-py
 echo "installed cairo-rs-py"
