9ea906705f3567f05d53fef8810fedc967e9396d
notV4l <122404722+notV4l@users.noreply.github.com>
2023-02-21 16:42:09 +0100

    Integrated devnet with rust vm (#319)
    [skip ci]

--- src/external-server/venv-devnet.ts
+++ src/external-server/venv-devnet.ts
@@ -6,6 +6,7 @@ import { ExternalServer } from "./external-server";
 export class VenvDevnet extends ExternalServer {
     private command: string;
     private args?: string[];
+    private vmLang?: string;
 
     constructor(
         venvPath: string,
@@ -13,12 +14,14 @@ export class VenvDevnet extends ExternalServer {
         port: string,
         args?: string[],
         stdout?: string,
-        stderr?: string
+        stderr?: string,
+        vmLang?: string
     ) {
         super(host, port, "is_alive", "integrated-devnet", stdout, stderr);
 
         this.command = "starknet-devnet";
         this.args = args;
+        this.vmLang = vmLang;
 
         if (venvPath !== "active") {
             this.command = getPrefixedCommand(normalizeVenvPath(venvPath), this.command);
@@ -27,7 +30,8 @@ export class VenvDevnet extends ExternalServer {
 
     protected async spawnChildProcess(): Promise<ChildProcess> {
         const args = ["--host", this.host, "--port", this.port].concat(this.args || []);
-        return spawn(this.command, args);
+        const options = { env: { PATH: process.env.PATH, STARKNET_DEVNET_CAIRO_VM: this.vmLang } };
+        return spawn(this.command, args, options);
     }
 
     protected cleanup(): void {
