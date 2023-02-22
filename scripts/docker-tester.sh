426e2cbcbd0b5f971dc4275c4715bc4cd56217f7
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-22 20:05:00 +0530

    [skip ci] Use deployed image

--- scripts/docker-tester.sh
+++ scripts/docker-tester.sh
@@ -1,4 +1,4 @@
-# docker build . -t shard-labs/starknet-hardhat-tester:0.1.0
+docker pull shramee/starknet-hardhat-dind-tester
 docker run --rm -ti \
 -e STARKNET_DEVNET=$(node -e "console.log(require('./config.json').STARKNET_DEVNET)") \
 -e CAIRO_LANG=$(node -e "console.log(require('./config.json').CAIRO_LANG)") \
@@ -6,4 +6,4 @@ docker run --rm -ti \
 -e STARKNET_HARDHAT_DIND_HOST_PATH="/home/circleci/project:$(pwd)" \
 -v $(pwd):/home/circleci/project \
 -v /var/run/docker.sock:/var/run/docker.sock \
---entrypoint bash shard-labs/starknet-hardhat-tester:0.1.0
+--entrypoint bash shramee/starknet-hardhat-dind-tester
