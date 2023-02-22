ad1572d7be6aafb2862bc2463a4549f2df5dc447
Nathan-SL <nl@shardlabs.io>
2023-02-22 17:31:54 +0300

    Allow users to pass nonce on deploy

--- src/account.ts
+++ src/account.ts
@@ -131,7 +131,8 @@ export abstract class Account {
                 calldata: adaptedArgs
             },
             {
-                maxFee: options?.maxFee
+                maxFee: options?.maxFee,
+                nonce: options?.nonce
             }
         );
 
