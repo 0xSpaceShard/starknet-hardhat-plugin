42f4824a7c9a9b6d71e4a69d03e7e3161f31548d
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-22 14:53:34 +0530

    [skip ci] Docker DinD config
    And STARKNET_HARDHAT_REPLACE_HOST_PATH env var to let JS know about the volume paths that need changing.

--- scripts/docker-tester.sh
+++ scripts/docker-tester.sh
@@ -1,4 +1,9 @@
-docker build . -t shard-labs/starknet-hardhat-tester:0.1.0
+# docker build . -t shard-labs/starknet-hardhat-tester:0.1.0
 docker run --rm -ti \
--v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/home/circleci/project \
+-e STARKNET_DEVNET=$(node -e "console.log(require('./config.json').STARKNET_DEVNET)") \
+-e CAIRO_LANG=$(node -e "console.log(require('./config.json').CAIRO_LANG)") \
+--network host \
+-e STARKNET_HARDHAT_REPLACE_HOST_PATH="/home/circleci/project:$(pwd)" \
+-v $(pwd):/home/circleci/project \
+-v /var/run/docker.sock:/var/run/docker.sock \
 --entrypoint bash shard-labs/starknet-hardhat-tester:0.1.0
