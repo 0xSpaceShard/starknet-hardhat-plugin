71d046e2ed3db98cad9fcbbbbc04d6406ac6f31f
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-22 15:55:39 +0530

    [skip ci] No requirement met logs pip

--- scripts/install-devnet.sh
+++ scripts/install-devnet.sh
@@ -5,6 +5,6 @@ set -eu
 # Get default from config.json file
 STARKNET_DEVNET_DEFAULT=$(node -e "console.log(require('../config.json').STARKNET_DEVNET)")
 
-pip3 install "starknet-devnet==${STARKNET_DEVNET:=$STARKNET_DEVNET_DEFAULT}"
+pip3 install "starknet-devnet==${STARKNET_DEVNET:=$STARKNET_DEVNET_DEFAULT}" | grep --invert-match 'Requirement already satisfied:'
 STARKNET_DEVNET_PATH=$(which starknet-devnet)
 echo "starknet-devnet at: $STARKNET_DEVNET_PATH"
