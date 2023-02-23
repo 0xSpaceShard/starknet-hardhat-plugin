7f20ca554bf2cde4d5c2b998077bdf6d62705d0d
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-02-23 11:35:21 +0530

    [skip ci] Docker image tag

--- scripts/docker-tester.sh
+++ scripts/docker-tester.sh
@@ -1,4 +1,4 @@
-docker pull shramee/starknet-hardhat-dind-tester
+docker pull shramee/starknet-hardhat-dind-tester:0.1
 docker run --rm -ti \
 -e STARKNET_DEVNET=$(node -e "console.log(require('./config.json').STARKNET_DEVNET)") \
 -e CAIRO_LANG=$(node -e "console.log(require('./config.json').CAIRO_LANG)") \
@@ -6,4 +6,4 @@ docker run --rm -ti \
 -e STARKNET_HARDHAT_DIND_HOST_PATH="/home/circleci/project:$(pwd)" \
 -v $(pwd):/home/circleci/project \
 -v /var/run/docker.sock:/var/run/docker.sock \
---entrypoint bash shramee/starknet-hardhat-dind-tester
+--entrypoint bash shramee/starknet-hardhat-dind-tester:0.1
