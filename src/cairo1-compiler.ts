d6455a622c1ec9f041a11029621c947ef3a29142
Nathan-SL <nl@shardlabs.io>
2023-04-04 09:17:41 +0300

    Update cairo1-compiler with docker

--- src/cairo1-compiler.ts
+++ src/cairo1-compiler.ts
@@ -1,7 +1,8 @@
 import { ProcessResult } from "@nomiclabs/hardhat-docker";
 import shell from "shelljs";
-// import { Image } from "@nomiclabs/hardhat-docker";
-// import { DockerServer } from "./external-server/docker-server";
+import { Image } from "@nomiclabs/hardhat-docker";
+import { DockerServer } from "./external-server/docker-server";
+import { getFreePort } from "./external-server/external-server";
 
 export const exec = (args: string) => {
     const result = shell.exec(args);
@@ -12,26 +13,53 @@ export const exec = (args: string) => {
     } as ProcessResult;
 };
 
-// export class DockerCairo1Compiler extends DockerServer {
-//     constructor(
-//         image: Image,
-//         host: string,
-//         port: string,
-//         private cairo1CompilerArgs?: string[],
-//         stdout?: string,
-//         stderr?: string,
-//     ) {
-//         super(image, host, port, "is_alive", "integrated-devnet", cairo1CompilerArgs, stdout, stderr);
-//     }
+export class DockerCairo1Compiler extends DockerServer {
+    constructor(
+        image: Image,
+        private sources: string[],
+        private cairo1CompilerArgs?: string[],
+        stdout?: string,
+        stderr?: string
+    ) {
+        super(
+            image,
+            "127.0.0.1",
+            null,
+            "",
+            "starknet-docker-cairo1-compiler",
+            cairo1CompilerArgs,
+            stdout,
+            stderr
+        );
+    }
 
-//     protected async getDockerArgs(): Promise<string[]> {
-//         return [
-//             "-p",
-//             `${this.host}:${this.port}:${this.port}`,
-//         ];
-//     }
+    protected async getDockerArgs(): Promise<string[]> {
+        const volumes = [];
+        for (const source of this.sources) {
+            volumes.push("-v", `${source}:${source}`);
+        }
 
-//     protected async getContainerArgs(): Promise<string[]> {
-//         return this.cairo1CompilerArgs || [];
-//     }
-// }
+        const dockerArgs = [...volumes];
+
+        if (this.isDockerDesktop) {
+            this.port = await this.getPort();
+            dockerArgs.push("-p", `${this.port}:${this.port}`);
+        } else {
+            dockerArgs.push("--network", "host");
+        }
+
+        return dockerArgs;
+    }
+
+    protected async getContainerArgs(): Promise<string[]> {
+        this.port = await this.getPort();
+        return ["/bin/sh", "-c", `"${this.cairo1CompilerArgs.join(" ")}"`];
+    }
+
+    protected async getPort(): Promise<string> {
+        if (!this.port) {
+            this.port = await getFreePort();
+        }
+        return this.port;
+    }
+}
