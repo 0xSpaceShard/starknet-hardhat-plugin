ac1a8799bc4976e641c76fed64a7c9fe82f065ff
Nathan-SL <nl@shardlabs.io>
2023-04-12 11:47:31 +0300

    Add extra test

---
+++ test/configuration-tests/with-cairo1-manifest-path/check.ts
@@ -0,0 +1,11 @@
+import { hardhatStarknetCairo1Compile } from "../../utils/cli-functions";
+import { assertContains, assertExistence } from "../../utils/utils";
+
+hardhatStarknetCairo1Compile("cairo1-contracts/cairo1.cairo ".split(" "));
+assertExistence("starknet-artifacts/cairo1-contracts/cairo1.cairo/cairo1.json");
+assertExistence("starknet-artifacts/cairo1-contracts/cairo1.cairo/cairo1.casm");
+assertExistence("starknet-artifacts/cairo1-contracts/cairo1.cairo/cairo1_abi.json");
+
+// Assert cairo0 compilation failure
+const execution = hardhatStarknetCairo1Compile("contracts/contract.cairo".split(" "), true);
+assertContains(execution.stdout, "Error: Contract not found");
