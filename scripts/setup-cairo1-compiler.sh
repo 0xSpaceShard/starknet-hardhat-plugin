886446ad18bedf339b77e1c9ad023df0dc599d3a
Nathan-SL <nl@shardlabs.io>
2023-04-12 11:51:49 +0300

    Add cairo1-compiler to CI/CD

---
+++ scripts/setup-cairo1-compiler.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+if [ "$TEST_SUBDIR" == "configuration-tests" ]; then
+    CONFIG_PATH="./config.json"
+    CAIRO_1_COMPILER_TARGET_TAG=$(jq ".CAIRO_COMPILER" "$CONFIG_PATH")
+
+    echo "Installing cairo compiler $CAIRO_1_COMPILER_TARGET_TAG"
+    # need rust to install cairo-rs-py
+    if rustc --version; then
+        echo "rustc installed"
+    else
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+        source ~/.cargo/env
+    fi
+
+    # setup cairo1 compiler
+    if [ -z "$CAIRO_1_COMPILER_MANIFEST" ]; then
+        mkdir cairo-compiler
+        git clone git@github.com:starkware-libs/cairo.git cairo-compiler \
+            --branch $CAIRO_1_COMPILER_TARGET_TAG \
+            --single-branch
+        CAIRO_1_COMPILER_MANIFEST="cairo-compiler/Cargo.toml"
+
+        if [ -n "$CIRCLE_BRANCH" ]; then
+            # needed by further testing steps
+            echo "export CAIRO_1_COMPILER_MANIFEST=$CAIRO_1_COMPILER_MANIFEST" >>"$BASH_ENV"
+            echo "source ~/.cargo/env" >>"$BASH_ENV"
+        fi
+    fi
+fi
