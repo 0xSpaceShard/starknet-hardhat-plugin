e481406c64013f95d2152c2c0ca8f3ff2c906ede
Nathan-SL <nl@shardlabs.io>
2023-04-19 15:47:24 +0300

    Uncomment vmlang-rust test

--- test/integrated-devnet-tests/with-docker-vmlang-rust/check.ts
+++ test/integrated-devnet-tests/with-docker-vmlang-rust/check.ts
@@ -1,10 +1,10 @@
-// import { hardhatStarknetCompileDeprecated, hardhatStarknetTest } from "../../utils/cli-functions";
-// import { checkDevnetIsNotRunning, assertContains } from "../../utils/utils";
+import { hardhatStarknetCompileDeprecated, hardhatStarknetTest } from "../../utils/cli-functions";
+import { checkDevnetIsNotRunning, assertContains } from "../../utils/utils";
 
-// (async () => {
-//     await checkDevnetIsNotRunning();
-//     hardhatStarknetCompileDeprecated(["contracts/contract.cairo"]);
-//     const execution = hardhatStarknetTest("--no-compile test/integrated-devnet.test.ts".split(" "));
-//     assertContains(execution.stderr, "Using Cairo VM: Rust");
-//     await checkDevnetIsNotRunning();
-// })();
+(async () => {
+    await checkDevnetIsNotRunning();
+    hardhatStarknetCompileDeprecated(["contracts/contract.cairo"]);
+    const execution = hardhatStarknetTest("--no-compile test/integrated-devnet.test.ts".split(" "));
+    assertContains(execution.stderr, "Using Cairo VM: Rust");
+    await checkDevnetIsNotRunning();
+})();
