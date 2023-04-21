f6c4f9a831bea60a0b6a53f6e1d0a14bb66c498c
Nathan-SL <nl@shardlabs.io>
2023-04-21 15:49:06 +0300

    Rename contract name [skip ci]

--- test/configuration-tests/with-cairo1-manifest-path/check.ts
+++ test/configuration-tests/with-cairo1-manifest-path/check.ts
@@ -2,10 +2,10 @@ import { hardhatStarknetCompile } from "../../utils/cli-functions";
 import { assertContains, assertExistence, ensureEnvVar } from "../../utils/utils";
 
 ensureEnvVar("CAIRO_1_COMPILER_MANIFEST");
-hardhatStarknetCompile("cairo1-contracts/cairo1.cairo ".split(" "));
-assertExistence("starknet-artifacts/cairo1-contracts/cairo1.cairo/cairo1.json");
-assertExistence("starknet-artifacts/cairo1-contracts/cairo1.cairo/cairo1.casm");
-assertExistence("starknet-artifacts/cairo1-contracts/cairo1.cairo/cairo1_abi.json");
+hardhatStarknetCompile("cairo1-contracts/contract1.cairo ".split(" "));
+assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1.json");
+assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1.casm");
+assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1_abi.json");
 
 // Assert cairo0 compilation failure
 const execution = hardhatStarknetCompile("contracts/contract.cairo".split(" "), true);
