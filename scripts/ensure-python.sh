f1c11ec557807f70d4ff6595a4238836f51c31eb
Nathan-SL <nl@shardlabs.io>
2023-04-28 17:07:17 +0300

    Install python for mac separately

--- scripts/ensure-python.sh
+++ scripts/ensure-python.sh
@@ -6,7 +6,12 @@ set -eu
 
 PY_VERSION=3.9.10
 
-if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     which "/opt/circleci/.pyenv/versions/$PY_VERSION/bin/python" || pyenv install "$PY_VERSION"
+    pyenv global "$PY_VERSION"
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    which "/Users/distiller/.pyenv/versions/$PY_VERSION/bin/python" || pyenv install "$PY_VERSION"
     pyenv global "$PY_VERSION"
 fi
