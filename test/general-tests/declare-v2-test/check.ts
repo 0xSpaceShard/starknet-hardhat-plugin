80064b66616cf63d49045020d8fab47513bcbe9f
FabijanC <fabijan.corak@gmail.com>
2023-05-03 16:27:59 +0200

    Fix compilation in declare-v2-test

--- test/general-tests/declare-v2-test/check.ts
+++ test/general-tests/declare-v2-test/check.ts
@@ -1,4 +1,4 @@
 import { hardhatStarknetCompile, hardhatStarknetTest } from "../../utils/cli-functions";
 
-hardhatStarknetCompile("cairo1-contracts/contract1.cairo".split(" "));
-hardhatStarknetTest("--no-compile test/declare-v2.test.ts".split(" "));
+hardhatStarknetCompile(["cairo1-contracts/contract1.cairo", "--add-pythonic-hints"]);
+hardhatStarknetTest(["--no-compile test/declare-v2.test.ts"]);
