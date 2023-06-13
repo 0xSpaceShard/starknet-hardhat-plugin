81d8dd0f8431b12eac70fcdd226edc426bae3432
Nathan-SL <nl@shardlabs.io>
2023-06-13 13:18:41 +0300

    Update test

--- test/general-tests/constructor-test/check.ts
+++ test/general-tests/constructor-test/check.ts
@@ -7,32 +7,31 @@ const prefix = path.join(__dirname);
 const sourcesPath = "cairo1-contracts";
 
 const duplicateConstructorContract = "duplicate_constructor.cairo";
-const contract1Path = path.join(sourcesPath, duplicateConstructorContract);
-
 const noConstructorContract = "no_constructor.cairo";
-const contract2Path = path.join(sourcesPath, noConstructorContract);
-
 const muteConstructorContract = "mute_constructor.cairo";
-const contract3Path = path.join(sourcesPath, muteConstructorContract);
-
 const commentedConstructorContract = "commented_constructor.cairo";
-const contract4Path = path.join(sourcesPath, commentedConstructorContract);
-
 const emptyLineConstructorContract = "empty_line_constructor.cairo";
-const contract5Path = path.join(sourcesPath, emptyLineConstructorContract);
+
+const contractNames = [
+    duplicateConstructorContract,
+    noConstructorContract,
+    muteConstructorContract,
+    commentedConstructorContract,
+    emptyLineConstructorContract
+];
 
 // Copy contracts to example repo to ensure files exists
-copyFileSync(path.join(prefix, duplicateConstructorContract), contract1Path);
-copyFileSync(path.join(prefix, noConstructorContract), contract2Path);
-copyFileSync(path.join(prefix, muteConstructorContract), contract3Path);
-copyFileSync(path.join(prefix, commentedConstructorContract), contract4Path);
-copyFileSync(path.join(prefix, emptyLineConstructorContract), contract5Path);
+for (const contractName of contractNames) {
+    const contractPath = path.join(sourcesPath, muteConstructorContract);
+    copyFileSync(path.join(prefix, contractName), contractPath);
+}
 
+const contract1Path = path.join(sourcesPath, duplicateConstructorContract);
 const expectedErrorMsg = "Error: Expected at most one constructor.";
 const execution = hardhatStarknetCompile([contract1Path], true);
 assertContains(execution.stderr, expectedErrorMsg);
 rmrfSync(contract1Path);
 
 // Compile cairo1 contracts
-hardhatStarknetCompile(["cairo1-contracts/", "--add-pythonic-hints"]);
+hardhatStarknetCompile([sourcesPath, "--add-pythonic-hints"]);
 hardhatStarknetTest("--no-compile test/constructor.test.ts".split(" "));
--- test/general-tests/constructor-test/no_constructor.cairo
+++ test/general-tests/constructor-test/no_constructor.cairo
@@ -2,11 +2,9 @@
 mod Contract {
 
     struct Storage {
-        balance: felt252,
     }
 
     #[external]
-    fn increase_balance(amount1: felt252, amount2: felt252) {
-        balance::write(balance::read() + amount1 + amount2);
+    fn increase_balance() {
     }
 }
