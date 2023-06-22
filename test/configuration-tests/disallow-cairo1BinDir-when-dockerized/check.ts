bcd8363a1130af7e4b8f00d80b52f6e4bfd62d1e
FabijanC <fabijan.corak@gmail.com>
2023-06-22 13:25:45 +0200

    Suspend configuration-tests/disallow-cairo1BinDir-when-dockerized

--- test/configuration-tests/disallow-cairo1BinDir-when-dockerized/check.ts
+++ test/configuration-tests/disallow-cairo1BinDir-when-dockerized/check.ts
@@ -1,5 +1,15 @@
+import { exit } from "process";
 import { hardhatStarknetTest } from "../../utils/cli-functions";
 import { assertContains } from "../../utils/utils";
 
+console.log(
+    "This test is suspended and can be removed in the future when we change how compiler version is selected"
+);
+console.log(
+    "Read more here:",
+    "https://github.com/0xSpaceShard/starknet-hardhat-plugin/issues/384"
+);
+
+exit(0);
 const execution = hardhatStarknetTest(["--no-compile", "test/quick-test.ts"], true);
 assertContains(execution.stderr, "cairo1BinDir cannot be used with dockerized plugin");
