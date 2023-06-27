bb001998864e43df92116799372c5fe7a60a51f7
FabijanC <fabijan.corak@gmail.com>
2023-06-27 17:47:16 +0200

    Export types; correct error prototype (#394)
    * [skip ci]

--- src/starknet-plugin-error.ts
+++ src/starknet-plugin-error.ts
@@ -4,5 +4,6 @@ import { PLUGIN_NAME } from "./constants";
 export class StarknetPluginError extends HardhatPluginError {
     constructor(message: string, parentError?: Error) {
         super(PLUGIN_NAME, message, parentError);
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
