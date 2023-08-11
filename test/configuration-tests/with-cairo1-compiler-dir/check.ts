51022cf36b892d2d98640a191bd02dcbd220eeae
FabijanC <fabijan.corak@gmail.com>
2023-08-11 15:44:07 +0200

    Add missing --single-file in test

--- test/configuration-tests/with-cairo1-compiler-dir/check.ts
+++ test/configuration-tests/with-cairo1-compiler-dir/check.ts
@@ -2,7 +2,7 @@ import { hardhatStarknetCompile } from "../../utils/cli-functions";
 import { assertContains, assertExistence, ensureEnvVar } from "../../utils/utils";
 
 ensureEnvVar("CAIRO_1_COMPILER_DIR");
-hardhatStarknetCompile(["cairo1-contracts/contract1.cairo"]);
+hardhatStarknetCompile(["cairo1-contracts/contract1.cairo", "--single-file"]);
 assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1.json");
 assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1.casm");
 assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1_abi.json");
