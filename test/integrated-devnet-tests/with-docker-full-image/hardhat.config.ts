859e6471e6b3e496db623fcf1d9bf1e728e9e91d
FabijanC <fabijan.corak@gmail.com>
2023-09-06 12:19:30 +0200

    Add test

---
+++ test/integrated-devnet-tests/with-docker-full-image/hardhat.config.ts
@@ -0,0 +1,16 @@
+import "@shardlabs/starknet-hardhat-plugin";
+
+module.exports = {
+    starknet: {
+        network: process.env.NETWORK
+    },
+    networks: {
+        integratedDevnet: {
+            dockerizedVersion: process.env.STARKNET_DEVNET,
+            url: "http://127.0.0.1:5050"
+        }
+    },
+    mocha: {
+        timeout: 1 // ms
+    }
+};
