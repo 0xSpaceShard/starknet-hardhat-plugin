62a7c52bee6f88df32e40de3a9800a965120e577
FabijanC <fabijan.corak@gmail.com>
2023-09-11 10:27:05 +0200

    Add --single-file where missing in tests

--- test/configuration-tests/with-compiler-version/check.ts
+++ test/configuration-tests/with-compiler-version/check.ts
@@ -1,7 +1,7 @@
 import { hardhatStarknetCompile } from "../../utils/cli-functions";
 import { assertExistence } from "../../utils/utils";
 
-hardhatStarknetCompile(["cairo1-contracts/contract1.cairo"]);
+hardhatStarknetCompile(["cairo1-contracts/contract1.cairo", "--single-file"]);
 assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1.json");
 assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1.casm");
 assertExistence("starknet-artifacts/cairo1-contracts/contract1.cairo/contract1_abi.json");
