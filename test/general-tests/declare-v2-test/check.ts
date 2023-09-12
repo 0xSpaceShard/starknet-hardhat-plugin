f6145a609bc6df15e6ab11443324251873a01212
FabijanC <fabijan.corak@gmail.com>
2023-09-12 17:08:33 +0200

    Restore missing pythonic hints in declareV2 test

--- test/general-tests/declare-v2-test/check.ts
+++ test/general-tests/declare-v2-test/check.ts
@@ -2,5 +2,9 @@ import { hardhatStarknetCompile, hardhatStarknetTest } from "../../utils/cli-fun
 import { ensureEnvVar } from "../../utils/utils";
 
 ensureEnvVar("CAIRO_1_COMPILER_DIR");
-hardhatStarknetCompile(["cairo1-contracts/contract1.cairo", "--single-file"]);
+hardhatStarknetCompile([
+    "cairo1-contracts/contract1.cairo",
+    "--add-pythonic-hints",
+    "--single-file"
+]);
 hardhatStarknetTest(["--no-compile test/cairo1/declare-v2.test.ts"]);
